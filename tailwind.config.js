b9e13d43f5692763ca39d2f1d9af62e498328a25
Rameshwor <r.sresta07@gmail.com>
2024-12-21 10:01:08 +0545

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* Add this in tailwind.config.js*/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
