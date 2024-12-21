22daf781edb242f0229f26e39820acd23f696e70
Rameshwor <r.sresta07@gmail.com>
2024-12-21 11:23:01 +0545

    [fixes]: landing

---
+++ src/components/LoginRegister/SignUp.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+const SignUp = () => {
+    return (
+        <div className="bg-white rounded-lg py-5">
+            <div className="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
+                <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
+                    <div className="flex items-center justify-center w-full lg:p-12">
+                        <div className="flex items-center xl:p-10">
+                            <form className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl">
+                                <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">
+                                    Sign Up
+                                </h3>
+                                <p className="mb-4 text-grey-700">
+                                    Enter your username, email and password
+                                </p>
+
+                                <label
+                                    htmlFor="username"
+                                    className="mb-2 text-sm text-start text-grey-900"
+                                >
+                                    Username*
+                                </label>
+                                <input
+                                    id="username"
+                                    type="text"
+                                    placeholder="Enter a username"
+                                    className="flex items-center w-full px-5 py-3 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                />
+                                <label
+                                    htmlFor="email"
+                                    className="mb-2 text-sm text-start text-grey-900"
+                                >
+                                    Email*
+                                </label>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    placeholder="mail@loopple.com"
+                                    className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                />
+                                <label
+                                    htmlFor="password"
+                                    className="mb-2 text-sm text-start text-grey-900"
+                                >
+                                    Password*
+                                </label>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    placeholder="Enter a password"
+                                    className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                />
+                                <label
+                                    htmlFor="confirm-password"
+                                    className="mb-2 text-sm text-start text-grey-900"
+                                >
+                                    Confirm Password*
+                                </label>
+                                <input
+                                    id="confirm-password"
+                                    type="password"
+                                    placeholder="Enter a password again"
+                                    className="flex items-center w-full px-5 py-5 mb-0 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                                />
+                                <div className="flex flex-row justify-between mb-8"></div>
+
+                                <div className="flex items-center mb-3">
+                                    <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                                    <p className="mx-4 text-grey-600">or</p>
+                                    <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                                </div>
+
+                                <link
+                                    className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300"
+                                    href="#"
+                                >
+                                    <img
+                                        className="h-5 mr-2"
+                                        src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
+                                        alt="Google Logo"
+                                    />
+                                    Sign in with Google
+                                </link>
+                                <button className="w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-red-500 focus:ring-4 focus:ring-purple-blue-100 bg-black">
+                                    Sign In
+                                </button>
+                                <p className="text-sm leading-relaxed text-grey-900">
+                                    Not registered yet?{" "}
+                                    <link
+                                        href="#"
+                                        className="font-bold text-grey-700"
+                                    >
+                                        Create an Account
+                                    </link>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
--- src/components/modules/Landing.js
+++ src/components/modules/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "../CardComponents/Categories";
 
 const LandingPage = () => {
     return (
@@ -8,25 +9,11 @@ const LandingPage = () => {
                 <h1 className="text-4xl font-bold flex justify-center">
                     Welcome to Newa Quiz
                 </h1>
-                <h2 className="text-2xl font-semibold flex justify-center my-12">
+                {/* <h2 className="text-2xl font-semibold flex justify-center my-12">
                     Categories
-                </h2>
-                <section className="grid grid-cols-3 gap-4 place-items-center text-center">
-                    <a href="">
-                        <div>
-                            <h3>Greeting</h3>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div>
-                            <h3>Foods</h3>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div>
-                            <h3>Dress</h3>
-                        </div>
-                    </a>
+                </h2> */}
+                <section className="">
+                    <Categories />
                 </section>
             </main>
         </div>
