ecaf7bfcdb44e6afbad8487769f1da79e0bf0210
Rameshwor <r.sresta07@gmail.com>
2024-12-22 11:06:14 +0545

    push

--- src/pages/CardComponents/Card.js
+++ src/pages/CardComponents/Card.js
@@ -9,10 +9,12 @@ const Card = ({ title, image, description }) => {
     return (
         <div className="bg-green-600 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 h-[20rem] flex flex-col justify-between">
             <div className="text-center">
-                <h3 className="text-4xl font-semibold mb-2 text-white">{title}</h3>
+                <h3 className="text-4xl font-semibold mb-2 text-white">
+                    {title}
+                </h3>
                 <img
                     // src={image}
-                    src="/icon/logo.png"
+                    src="/icon/icons.png"
                     alt={title}
                     className="mx-auto mb-3 h-[12rem] object-contain"
                 />
